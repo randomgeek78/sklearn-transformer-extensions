a6b3423ce662044a80192a6ad5e255214db263d5
Random Geek <randomgeek78@gmail.com>
2022-03-10 18:20:10 -0800

    Exposed ColumnTransformer

--- sklearn_transformer_extensions/__init__.py
+++ sklearn_transformer_extensions/__init__.py
@@ -3,9 +3,11 @@ __version__ = '0.1.7'
 from .transformers import XyAdapter
 from .transformers import make_column_transformer
 from .transformers import FunctionTransformer
+from .transformers import ColumnTransformer
 
 __all__ = [
     "XyAdapter",
     "make_column_transformer",
     "FunctionTransformer",
+    "ColumnTransformer",
 ]
